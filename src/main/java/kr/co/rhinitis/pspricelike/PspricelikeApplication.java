c575943aaca6bd2e2927f168429af2eac7bc1a02
jyj2187 <jyjgom@gmail.com>
2022-08-28 17:03:53 +0900

    [add] 프로젝트 생성

---
+++ src/main/java/kr/co/rhinitis/pspricelike/PspricelikeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.rhinitis.pspricelike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspricelikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PspricelikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/rhinitis/pspricelike/PspricelikeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.rhinitis.pspricelike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspricelikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
